836c568cdce6285c46e99f51f035bb1a93a60f4c
yokoyamamasahiro <part7steelballrun.to.8@gmail.com>
2018-10-19 20:56:31 +0900

    solve a task scheduling problem

--- atcoder/2-2/task_scheduling/ABC103/d.go
+++ atcoder/2-2/task_scheduling/ABC103/d.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -37,7 +39,42 @@ func NextRunesLine() []rune {
 /*******************************************************************/
 
 var n, m int
-var A, B []int
+
+type bridge struct {
+	start, end int
+}
+type Interface []*bridge
+
+func (b Interface) Len() int {
+	return len(b)
+}
+func (b Interface) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+func (b Interface) Less(i, j int) bool {
+	return b[i].end < b[j].end
+}
 
 func main() {
+	tmp := NextIntsLine()
+	n, m = tmp[0], tmp[1]
+	bridgeSlice := []*bridge{}
+	for i := 0; i < m; i++ {
+		tmp = NextIntsLine()
+		bridgeSlice = append(bridgeSlice, &bridge{start: tmp[0], end: tmp[1]})
+	}
+	sort.Sort(Interface(bridgeSlice))
+
+	answer := 0
+	prev := -1
+	for _, b := range bridgeSlice {
+		// すでに侵略不可となっている場合はスキップ
+		if b.start <= prev {
+			continue
+		}
+		// 橋を壊す
+		prev = b.end - 1
+		answer++
+	}
+	fmt.Println(answer)
 }
