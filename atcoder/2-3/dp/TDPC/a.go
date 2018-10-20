01a7e67bfc2406ddfeb6061766eecd9fbaf20db3
yokoyamamasahiro <part7steelballrun.to.8@gmail.com>
2018-10-20 15:00:36 +0900

    solve partial sum dp problem

--- atcoder/2-3/dp/TDPC/a.go
+++ atcoder/2-3/dp/TDPC/a.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -66,5 +67,35 @@ func Min(integers ...int) int {
 
 /*******************************************************************/
 
+var n int
+var p []int
+var dp [110][100*100 + 10]bool // dp[i][j]: i番目以降の問題集合から任意個解く問題を選んだとき、j点を取れるかどうか
+
 func main() {
+	tmp := NextIntsLine()
+	n = tmp[0]
+	p = NextIntsLine()
+
+	// 初期化
+	for i := 0; i < n+1; i++ {
+		dp[i][0] = true
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := 0; j <= 100*n; j++ {
+			if j >= p[i] {
+				dp[i][j] = dp[i+1][j] || dp[i+1][j-p[i]]
+			} else {
+				dp[i][j] = dp[i+1][j]
+			}
+		}
+	}
+
+	answer := 0
+	for j := 0; j <= 100*n; j++ {
+		if dp[0][j] {
+			answer++
+		}
+	}
+	fmt.Println(answer)
 }
