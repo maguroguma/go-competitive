1791077f22e7d15d49762c8586244825caf104a0
yokoyamamasahiro <part7steelballrun.to.8@gmail.com>
2018-11-01 23:09:33 +0900

    solve some past contests

---
+++ past_contests/ABC057/b.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+// NextLine reads a line text from stdin, and then returns its string.
+func NextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// NextIntsLine reads a line text, that consists of **ONLY INTEGERS DELIMITED BY SPACES**, from stdin.
+// And then returns intergers slice.
+func NextIntsLine() []int {
+	ints := []int{}
+	intsStr := NextLine()
+	tmp := strings.Split(intsStr, " ")
+	for _, s := range tmp {
+		integer, _ := strconv.Atoi(s)
+		ints = append(ints, integer)
+	}
+	return ints
+}
+
+// NextRunesLine reads a line text, that consists of **ONLY CHARACTERS ARRANGED CONTINUOUSLY**, from stdin.
+// Ant then returns runes slice.
+func NextRunesLine() []rune {
+	return []rune(NextLine())
+}
+
+// Max returns the max integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func Max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// Min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func Min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// PowInt is integer version of math.Pow
+func PowInt(a, e int) int {
+	if a < 0 || e < 0 {
+		panic(errors.New("[argument error]: PowInt does not accept negative integers"))
+	}
+	fa := float64(a)
+	fe := float64(e)
+	fanswer := math.Pow(fa, fe)
+	return int(fanswer)
+}
+
+// AbsInt is integer version of math.Abs
+func AbsInt(a int) int {
+	fa := float64(a)
+	fanswer := math.Abs(fa)
+	return int(fanswer)
+}
+
+// DeleteElement returns a *NEW* slice, that have the same and minimum length and capacity.
+// DeleteElement makes a new slice by using easy slice literal.
+func DeleteElement(s []int, i int) []int {
+	if i < 0 || len(s) <= i {
+		panic(errors.New("[index error]"))
+	}
+	// appendのみの実装
+	n := make([]int, 0, len(s)-1)
+	n = append(n, s[:i]...)
+	n = append(n, s[i+1:]...)
+	return n
+}
+
+// Concat returns a *NEW* slice, that have the same and minimum length and capacity.
+func Concat(s, t []rune) []rune {
+	n := make([]rune, 0, len(s)+len(t))
+	n = append(n, s...)
+	n = append(n, t...)
+	return n
+}
+
+// sort package (snippets)
+//sort.Sort(sort.IntSlice(s))
+//sort.Sort(sort.Reverse(sort.IntSlice(s)))
+//sort.Sort(sort.Float64Slice(s))
+//sort.Sort(sort.StringSlice(s))
+
+// copy function
+//a = []int{0, 1, 2}
+//b = make([]int, len(a))
+//copy(b, a)
+
+/*******************************************************************/
+
+var n, m int
+var A, B, C, D []int
+
+func main() {
+	tmp := NextIntsLine()
+	n, m = tmp[0], tmp[1]
+	for i := 0; i < n; i++ {
+		tmp = NextIntsLine()
+		A = append(A, tmp[0])
+		B = append(B, tmp[1])
+	}
+	for i := 0; i < m; i++ {
+		tmp = NextIntsLine()
+		C = append(C, tmp[0])
+		D = append(D, tmp[1])
+	}
+
+	// 各学生
+	for i := 0; i < n; i++ {
+		sx, sy := A[i], B[i]
+		// 各チェックポイント
+		minDist := 1000000000
+		number := -1
+		for j := 0; j < m; j++ {
+			tx, ty := C[j], D[j]
+			dist := AbsInt(sx-tx) + AbsInt(sy-ty)
+			if dist < minDist {
+				minDist = dist
+				number = j + 1
+			}
+		}
+		fmt.Println(number)
+	}
+}
---
+++ past_contests/ABC057/c.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+// NextLine reads a line text from stdin, and then returns its string.
+func NextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// NextIntsLine reads a line text, that consists of **ONLY INTEGERS DELIMITED BY SPACES**, from stdin.
+// And then returns intergers slice.
+func NextIntsLine() []int {
+	ints := []int{}
+	intsStr := NextLine()
+	tmp := strings.Split(intsStr, " ")
+	for _, s := range tmp {
+		integer, _ := strconv.Atoi(s)
+		ints = append(ints, integer)
+	}
+	return ints
+}
+
+// NextRunesLine reads a line text, that consists of **ONLY CHARACTERS ARRANGED CONTINUOUSLY**, from stdin.
+// Ant then returns runes slice.
+func NextRunesLine() []rune {
+	return []rune(NextLine())
+}
+
+// Max returns the max integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func Max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// Min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func Min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// PowInt is integer version of math.Pow
+func PowInt(a, e int) int {
+	if a < 0 || e < 0 {
+		panic(errors.New("[argument error]: PowInt does not accept negative integers"))
+	}
+	fa := float64(a)
+	fe := float64(e)
+	fanswer := math.Pow(fa, fe)
+	return int(fanswer)
+}
+
+// AbsInt is integer version of math.Abs
+func AbsInt(a int) int {
+	fa := float64(a)
+	fanswer := math.Abs(fa)
+	return int(fanswer)
+}
+
+// DeleteElement returns a *NEW* slice, that have the same and minimum length and capacity.
+// DeleteElement makes a new slice by using easy slice literal.
+func DeleteElement(s []int, i int) []int {
+	if i < 0 || len(s) <= i {
+		panic(errors.New("[index error]"))
+	}
+	// appendのみの実装
+	n := make([]int, 0, len(s)-1)
+	n = append(n, s[:i]...)
+	n = append(n, s[i+1:]...)
+	return n
+}
+
+// Concat returns a *NEW* slice, that have the same and minimum length and capacity.
+func Concat(s, t []rune) []rune {
+	n := make([]rune, 0, len(s)+len(t))
+	n = append(n, s...)
+	n = append(n, t...)
+	return n
+}
+
+// sort package (snippets)
+//sort.Sort(sort.IntSlice(s))
+//sort.Sort(sort.Reverse(sort.IntSlice(s)))
+//sort.Sort(sort.Float64Slice(s))
+//sort.Sort(sort.StringSlice(s))
+
+// copy function
+//a = []int{0, 1, 2}
+//b = make([]int, len(a))
+//copy(b, a)
+
+/*******************************************************************/
+
+var n int
+
+func main() {
+	tmp := NextIntsLine()
+	n = tmp[0]
+	q := int(math.Pow(float64(n), 0.5))
+	answer := 100
+	for i := 1; i <= q; i++ {
+		if n%i != 0 {
+			continue
+		}
+		a := i
+		b := n / i
+		adigit := getDigit(a)
+		bdigit := getDigit(b)
+		f := Max(adigit, bdigit)
+		if f < answer {
+			answer = f
+		}
+	}
+	if answer == 100 {
+		answer = 1
+	}
+	fmt.Println(answer)
+}
+
+func getDigit(a int) int {
+	i := 1
+	for {
+		a /= 10
+		if a == 0 {
+			break
+		}
+		i++
+	}
+	return i
+}
