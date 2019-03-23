5ab05626290d8e7a58fca37a63e347bd84302b1b
myokoyama <part7steelballrun.to.8@gmail.com>
2019-03-24 03:10:34 +0900

    solve some graph problems

--- algorithms/util/util_test.go
+++ algorithms/util/util_test.go
@@ -23,3 +23,21 @@ func Test整数スライスの累積和スライス(t *testing.T) {
 	assert.Equal(t, []int{0, 1, 3, 6, 10, 15, 21, 28, 36, 45},
 		GetCumulativeSums([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}))
 }
+
+func Test累積和スライスを用いた部分和の算出(t *testing.T) {
+	A := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	sums := GetCumulativeSums(A)
+
+	// 半開区間として扱える
+	// ex.) 0<=m<=n のとき A の [m, n) の和は sums[n] - sums[m]
+	assert.Equal(t, A[0]+A[1]+A[2]+A[3], sums[4]-sums[0])
+	assert.Equal(t, A[3]+A[4]+A[5]+A[6], sums[7]-sums[3])
+	// コーナーケース（？）
+	assert.Equal(t, 45, sums[len(A)])
+
+	// 備忘録
+	// ↓これはセーフ
+	assert.Equal(t, []int{}, A[len(A):len(A)])
+	// ↓これはアウト
+	// assert.Equal(t, []int{}, A[len(A)+1:len(A)+1])
+}
