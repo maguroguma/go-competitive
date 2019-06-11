6d12daa1390f248ed68c88904b06c6c6fe959e78
myokoyama <part7steelballrun.to.8@gmail.com>
2019-06-11 23:05:17 +0900

    after ABC 129

---
+++ contests/20190609_ABC129/d/d.go
@@ -0,0 +1,423 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+/*********** I/O ***********/
+
+var (
+	// ReadString returns a WORD string.
+	ReadString func() string
+	stdout     *bufio.Writer
+)
+
+func init() {
+	ReadString = newReadString(os.Stdin)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+func newReadString(ior io.Reader) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+11))
+	// Split sets the split function for the Scanner. The default split function is ScanLines.
+	// Split panics if it is called after scanning has started.
+	r.Split(bufio.ScanWords)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// ReadInt returns an integer.
+func ReadInt() int {
+	return int(readInt64())
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(ReadString(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// ReadIntSlice returns an integer slice that has n integers.
+func ReadIntSlice(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt()
+	}
+	return b
+}
+
+// ReadFloat64 returns an float64.
+func ReadFloat64() float64 {
+	return float64(readFloat64())
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(ReadString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func ReadFloat64Slice(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadFloat64()
+	}
+	return b
+}
+
+// ReadRuneSlice returns a rune slice.
+func ReadRuneSlice() []rune {
+	return []rune(ReadString())
+}
+
+/*********** Debugging ***********/
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
+
+/*********** DP sub-functions ***********/
+
+// ChMin accepts a pointer of integer and a target value.
+// If target value is SMALLER than the first argument,
+//	then the first argument will be updated by the second argument.
+func ChMin(updatedValue *int, target int) bool {
+	if *updatedValue > target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// ChMax accepts a pointer of integer and a target value.
+// If target value is LARGER than the first argument,
+//	then the first argument will be updated by the second argument.
+func ChMax(updatedValue *int, target int) bool {
+	if *updatedValue < target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// NthBit returns nth bit value of an argument.
+// n starts from 0.
+func NthBit(num, nth int) int {
+	return num >> uint(nth) & 1
+}
+
+/*********** Arithmetic ***********/
+
+// Max returns the max integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func Max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// Min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func Min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// DigitSum returns digit sum of a decimal number.
+// DigitSum only accept a positive integer.
+func DigitSum(n int) int {
+	if n < 0 {
+		return -1
+	}
+
+	res := 0
+
+	for n > 0 {
+		res += n % 10
+		n /= 10
+	}
+
+	return res
+}
+
+// Sum returns multiple integers sum.
+func Sum(integers ...int) int {
+	s := 0
+
+	for _, i := range integers {
+		s += i
+	}
+
+	return s
+}
+
+// CeilInt returns the minimum integer larger than or equal to float(a/b).
+func CeilInt(a, b int) int {
+	res := a / b
+	if a%b > 0 {
+		res++
+	}
+	return res
+}
+
+// FloorInt returns the maximum integer smaller than or equal to float(a/b)
+func FloorInt(a, b int) int {
+	res := a / b
+	return res
+}
+
+// PowInt is integer version of math.Pow
+// PowInt calculate a power by Binary Power (二分累乗法(O(log e))).
+func PowInt(a, e int) int {
+	if a < 0 || e < 0 {
+		panic(errors.New("[argument error]: PowInt does not accept negative integers"))
+	}
+
+	if e == 0 {
+		return 1
+	}
+
+	if e%2 == 0 {
+		halfE := e / 2
+		half := PowInt(a, halfE)
+		return half * half
+	}
+
+	return a * PowInt(a, e-1)
+}
+
+// AbsInt is integer version of math.Abs
+func AbsInt(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+// Gcd returns the Greatest Common Divisor of two natural numbers.
+// Gcd only accepts two natural numbers (a, b >= 1).
+// 0 or negative number causes panic.
+// Gcd uses the Euclidean Algorithm.
+func Gcd(a, b int) int {
+	if a <= 0 || b <= 0 {
+		panic(errors.New("[argument error]: Gcd only accepts two NATURAL numbers"))
+	}
+	if a < b {
+		a, b = b, a
+	}
+
+	// Euclidean Algorithm
+	for b > 0 {
+		div := a % b
+		a, b = b, div
+	}
+
+	return a
+}
+
+// Lcm returns the Least Common Multiple of two natural numbers.
+// Lcd only accepts two natural numbers (a, b >= 1).
+// 0 or negative number causes panic.
+// Lcd uses the Euclidean Algorithm indirectly.
+func Lcm(a, b int) int {
+	if a <= 0 || b <= 0 {
+		panic(errors.New("[argument error]: Gcd only accepts two NATURAL numbers"))
+	}
+
+	// a = a'*gcd, b = b'*gcd, a*b = a'*b'*gcd^2
+	// a' and b' are relatively prime numbers
+	// gcd consists of prime numbers, that are included in a and b
+	gcd := Gcd(a, b)
+
+	// not (a * b / gcd), because of reducing a probability of overflow
+	return (a / gcd) * b
+}
+
+// Strtoi is a wrapper of `strconv.Atoi()`.
+// If `strconv.Atoi()` returns an error, Strtoi calls panic.
+func Strtoi(s string) int {
+	if i, err := strconv.Atoi(s); err != nil {
+		panic(errors.New("[argument error]: Strtoi only accepts integer string"))
+	} else {
+		return i
+	}
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+/********** I/O usage **********/
+
+//str := ReadString()
+//i := ReadInt()
+//X := ReadIntSlice(n)
+//S := ReadRuneSlice()
+
+/*******************************************************************/
+
+const MOD = 1000000000 + 7
+const ALPHABET_NUM = 26
+
+var h, w int
+var S [][]rune
+
+var yoko, tate [][]int
+
+func main() {
+	h, w = ReadInt(), ReadInt()
+	for i := 0; i < h; i++ {
+		row := ReadRuneSlice()
+		S = append(S, row)
+	}
+
+	for i := 0; i < h; i++ {
+		yoko = append(yoko, make([]int, w))
+		tate = append(tate, make([]int, w))
+	}
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if S[i][j] == '#' {
+				yoko[i][j], tate[i][j] = -1, -1
+			} else {
+				yoko[i][j], tate[i][j] = 1, 1
+			}
+		}
+	}
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if yoko[i][j] == -1 {
+				continue
+			}
+
+			if j+1 < w && yoko[i][j+1] != -1 {
+				yoko[i][j+1] += yoko[i][j]
+			}
+		}
+	}
+	for i := 0; i < h; i++ {
+		for j := w - 1; j >= 0; j-- {
+			if yoko[i][j] == -1 {
+				continue
+			}
+
+			if j-1 >= 0 && yoko[i][j-1] != -1 {
+				yoko[i][j-1] = yoko[i][j]
+			}
+		}
+	}
+	// for i := 0; i < h; i++ {
+	// 	fmt.Println(yoko[i])
+	// }
+
+	for j := 0; j < w; j++ {
+		for i := 0; i < h; i++ {
+			if tate[i][j] == -1 {
+				continue
+			}
+
+			if i+1 < h && tate[i+1][j] != -1 {
+				tate[i+1][j] += tate[i][j]
+			}
+		}
+	}
+	for j := 0; j < w; j++ {
+		for i := h - 1; i >= 0; i-- {
+			if tate[i][j] == -1 {
+				continue
+			}
+
+			if i-1 >= 0 && tate[i-1][j] != -1 {
+				tate[i-1][j] = tate[i][j]
+			}
+		}
+	}
+	// for i := 0; i < h; i++ {
+	// 	fmt.Println(tate[i])
+	// }
+
+	ans := 0
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if S[i][j] == '#' {
+				continue
+			}
+
+			if yoko[i][j] > 0 && tate[i][j] > 0 {
+				ChMax(&ans, yoko[i][j]+tate[i][j]-1)
+			} else {
+				ChMax(&ans, yoko[i][j]+tate[i][j])
+			}
+		}
+	}
+
+	fmt.Println(ans)
+}
+
+// MODはとったか？
+// 遷移だけじゃなくて最後の最後でちゃんと取れよ？
+
+/*******************************************************************/
