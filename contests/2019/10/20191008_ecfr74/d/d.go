e3f8c6d117d7e16fea02bd79096e628dd45291a1
myokoyama <part7steelballrun.to.8@gmail.com>
2019-12-09 01:11:05 +0900

    review ecfr74 D

--- contests/2019/10/20191008_ecfr74/d/d.go
+++ contests/2019/10/20191008_ecfr74/d/d.go
@@ -395,8 +395,76 @@ const ALPHABET_NUM = 26
 const INF_INT64 = math.MaxInt64
 const INF_BIT60 = 1 << 60
 
+var n int64
+var S []rune
+
 func main() {
-	fmt.Println("Hello World.")
+	n = ReadInt64()
+	S = ReadRuneSlice()
+
+	ans := n * (n - 1) / 2
+
+	pressed, counts := RunLengthEncoding(S)
+
+	length := len(pressed)
+	for i := 0; i < length; i++ {
+		if i+1 < length {
+			ans -= int64(counts[i+1])
+		}
+		if i-1 >= 0 {
+			ans -= int64(counts[i-1])
+		}
+	}
+	// 重複して減らした部分を補正
+	ans += int64(length - 1)
+
+	fmt.Println(ans)
+}
+
+// RunLengthEncoding returns encoded slice of an input.
+func RunLengthEncoding(S []rune) ([]rune, []int) {
+	runes := []rune{}
+	lengths := []int{}
+
+	l := 0
+	for i := 0; i < len(S); i++ {
+		// 1文字目の場合保持
+		if i == 0 {
+			l = 1
+			continue
+		}
+
+		if S[i-1] == S[i] {
+			// 直前の文字と一致していればインクリメント
+			l++
+		} else {
+			// 不一致のタイミングで追加し、長さをリセットする
+			runes = append(runes, S[i-1])
+			lengths = append(lengths, l)
+			l = 1
+		}
+	}
+	runes = append(runes, S[len(S)-1])
+	lengths = append(lengths, l)
+
+	return runes, lengths
+}
+
+// RunLengthDecoding decodes RLE results.
+func RunLengthDecoding(S []rune, L []int) []rune {
+	if len(S) != len(L) {
+		panic("S, L are not RunLengthEncoding results")
+	}
+
+	res := []rune{}
+
+	for i := 0; i < len(S); i++ {
+		for j := 0; j < L[i]; j++ {
+			res = append(res, S[i])
+		}
+	}
+
+	return res
 }
 
 // MODはとったか？
