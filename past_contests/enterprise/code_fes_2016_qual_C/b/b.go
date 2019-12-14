b5309c5044b6ef4be620bfe1afa00239146237e2
myokoyama <part7steelballrun.to.8@gmail.com>
2019-12-14 12:53:56 +0900

    solved some past problems

---
+++ past_contests/enterprise/code_fes_2016_qual_C/b/b.go
@@ -0,0 +1,319 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+/*********** I/O ***********/
+
+var (
+	// ReadString returns a WORD string.
+	ReadString func() string
+	stdout     *bufio.Writer
+)
+
+func init() {
+	ReadString = newReadString(os.Stdin)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+func newReadString(ior io.Reader) func() string {
+	r := bufio.NewScanner(ior)
+	// r.Buffer(make([]byte, 1024), int(1e+11)) // for AtCoder
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	// Split sets the split function for the Scanner. The default split function is ScanLines.
+	// Split panics if it is called after scanning has started.
+	r.Split(bufio.ScanWords)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// ReadInt returns an integer.
+func ReadInt() int {
+	return int(readInt64())
+}
+func ReadInt2() (int, int) {
+	return int(readInt64()), int(readInt64())
+}
+func ReadInt3() (int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64())
+}
+func ReadInt4() (int, int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64()), int(readInt64())
+}
+
+// ReadInt64 returns as integer as int64.
+func ReadInt64() int64 {
+	return readInt64()
+}
+func ReadInt64_2() (int64, int64) {
+	return readInt64(), readInt64()
+}
+func ReadInt64_3() (int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64()
+}
+func ReadInt64_4() (int64, int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64(), readInt64()
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(ReadString(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// ReadIntSlice returns an integer slice that has n integers.
+func ReadIntSlice(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt()
+	}
+	return b
+}
+
+// ReadInt64Slice returns as int64 slice that has n integers.
+func ReadInt64Slice(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt64()
+	}
+	return b
+}
+
+// ReadFloat64 returns an float64.
+func ReadFloat64() float64 {
+	return float64(readFloat64())
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(ReadString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func ReadFloat64Slice(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadFloat64()
+	}
+	return b
+}
+
+// ReadRuneSlice returns a rune slice.
+func ReadRuneSlice() []rune {
+	return []rune(ReadString())
+}
+
+/*********** Debugging ***********/
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
+
+// Strtoi is a wrapper of strconv.Atoi().
+// If strconv.Atoi() returns an error, Strtoi calls panic.
+func Strtoi(s string) int {
+	if i, err := strconv.Atoi(s); err != nil {
+		panic(errors.New("[argument error]: Strtoi only accepts integer string"))
+	} else {
+		return i
+	}
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintDebug is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func PrintDebug(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := ReadString()
+//i := ReadInt()
+//X := ReadIntSlice(n)
+//S := ReadRuneSlice()
+//a := ReadFloat64()
+//A := ReadFloat64Slice(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*******************************************************************/
+
+const MOD = 1000000000 + 7
+const ALPHABET_NUM = 26
+const INF_INT64 = math.MaxInt64
+const INF_BIT60 = 1 << 60
+
+var k, t int
+var A []int
+
+func main() {
+	k, t = ReadInt2()
+	A = ReadIntSlice(t)
+
+	temp := make(ItemPQ, 0)
+	pq := &temp
+	heap.Init(pq)
+	for i := 0; i < t; i++ {
+		heap.Push(pq, &Item{pri: A[i], id: i})
+	}
+
+	answers := []int{}
+	for pq.Len() > 0 {
+		fp := heap.Pop(pq).(*Item)
+
+		if len(answers) == 0 {
+			answers = append(answers, fp.id)
+			fp.pri--
+			if fp.pri > 0 {
+				heap.Push(pq, fp)
+			}
+			continue
+		}
+
+		if answers[len(answers)-1] != fp.id {
+			answers = append(answers, fp.id)
+			fp.pri--
+			if fp.pri > 0 {
+				heap.Push(pq, fp)
+			}
+			continue
+		}
+
+		if answers[len(answers)-1] == fp.id && pq.Len() > 0 {
+			sp := heap.Pop(pq).(*Item)
+			answers = append(answers, sp.id)
+			sp.pri--
+
+			heap.Push(pq, fp)
+			if sp.pri > 0 {
+				heap.Push(pq, sp)
+			}
+			continue
+		}
+
+		answers = append(answers, fp.id)
+		fp.pri--
+		if fp.pri > 0 {
+			heap.Push(pq, fp)
+		}
+	}
+
+	ans := 0
+	for i := 1; i < len(answers); i++ {
+		if answers[i] == answers[i-1] {
+			ans++
+		}
+	}
+	fmt.Println(ans)
+}
+
+type Item struct {
+	pri     int
+	id, num int
+}
+type ItemPQ []*Item
+
+func (pq ItemPQ) Len() int           { return len(pq) }
+func (pq ItemPQ) Less(i, j int) bool { return pq[i].pri > pq[j].pri } // <: ASC, >: DESC
+func (pq ItemPQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+func (pq *ItemPQ) Push(x interface{}) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+func (pq *ItemPQ) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+// how to use
+// temp := make(ItemPQ, 0, 100000+1)
+// pq := &temp
+// heap.Init(pq)
+// heap.Push(pq, &Item{pri: intValue})
+// popped := heap.Pop(pq).(*Item)
+
+/*
+- MODは最後にとりましたか？
+- ループを抜けた後も処理が必要じゃありませんか？
+- 和・積・あまりを求められたらint64が必要ではありませんか？
+- いきなりオーバーフローはしていませんか？
+*/
+
+/*******************************************************************/
