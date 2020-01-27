a62e90f24680e5489d6e5f2de1e96bdb066000cb
myokoyama <part7steelballrun.to.8@gmail.com>
2020-01-28 01:51:08 +0900

    done ABC153

---
+++ contests/2020/01/20200126_ABC153/f/f.go
@@ -0,0 +1,506 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+/*********** I/O ***********/
+
+var (
+	// ReadString returns a WORD string.
+	ReadString func() string
+	stdout     *bufio.Writer
+)
+
+func init() {
+	ReadString = newReadString(os.Stdin)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+func newReadString(ior io.Reader) func() string {
+	r := bufio.NewScanner(ior)
+	// r.Buffer(make([]byte, 1024), int(1e+11)) // for AtCoder
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	// Split sets the split function for the Scanner. The default split function is ScanLines.
+	// Split panics if it is called after scanning has started.
+	r.Split(bufio.ScanWords)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// ReadInt returns an integer.
+func ReadInt() int {
+	return int(readInt64())
+}
+func ReadInt2() (int, int) {
+	return int(readInt64()), int(readInt64())
+}
+func ReadInt3() (int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64())
+}
+func ReadInt4() (int, int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64()), int(readInt64())
+}
+
+// ReadInt64 returns as integer as int64.
+func ReadInt64() int64 {
+	return readInt64()
+}
+func ReadInt64_2() (int64, int64) {
+	return readInt64(), readInt64()
+}
+func ReadInt64_3() (int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64()
+}
+func ReadInt64_4() (int64, int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64(), readInt64()
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(ReadString(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// ReadIntSlice returns an integer slice that has n integers.
+func ReadIntSlice(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt()
+	}
+	return b
+}
+
+// ReadInt64Slice returns as int64 slice that has n integers.
+func ReadInt64Slice(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt64()
+	}
+	return b
+}
+
+// ReadFloat64 returns an float64.
+func ReadFloat64() float64 {
+	return float64(readFloat64())
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(ReadString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func ReadFloat64Slice(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadFloat64()
+	}
+	return b
+}
+
+// ReadRuneSlice returns a rune slice.
+func ReadRuneSlice() []rune {
+	return []rune(ReadString())
+}
+
+/*********** Debugging ***********/
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
+
+// Strtoi is a wrapper of strconv.Atoi().
+// If strconv.Atoi() returns an error, Strtoi calls panic.
+func Strtoi(s string) int {
+	if i, err := strconv.Atoi(s); err != nil {
+		panic(errors.New("[argument error]: Strtoi only accepts integer string"))
+	} else {
+		return i
+	}
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintDebug is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func PrintDebug(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := ReadString()
+//i := ReadInt()
+//X := ReadIntSlice(n)
+//S := ReadRuneSlice()
+//a := ReadFloat64()
+//A := ReadFloat64Slice(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*
+ASCII code
+
+ASCII   10進数  ASCII   10進数  ASCII   10進数
+!       33      "       34      #       35
+$       36      %       37      &       38
+'       39      (       40      )       41
+*       42      +       43      ,       44
+-       45      .       46      /       47
+0       48      1       49      2       50
+3       51      4       52      5       53
+6       54      7       55      8       56
+9       57      :       58      ;       59
+<       60      =       61      >       62
+?       63      @       64      A       65
+B       66      C       67      D       68
+E       69      F       70      G       71
+H       72      I       73      J       74
+K       75      L       76      M       77
+N       78      O       79      P       80
+Q       81      R       82      S       83
+T       84      U       85      V       86
+W       87      X       88      Y       89
+Z       90      [       91      \       92
+]       93      ^       94      _       95
+`       96      a       97      b       98
+c       99      d       100     e       101
+f       102     g       103     h       104
+i       105     j       106     k       107
+l       108     m       109     n       110
+o       111     p       112     q       113
+r       114     s       115     t       116
+u       117     v       118     w       119
+x       120     y       121     z       122
+{       123     |       124     }       125
+~       126             127
+*/
+
+/*******************************************************************/
+
+const (
+	// General purpose
+	MOD          = 1000000000 + 7
+	ALPHABET_NUM = 26
+	INF_INT64    = math.MaxInt64
+	INF_BIT60    = 1 << 60
+	INF_INT32    = math.MaxInt32
+	INF_BIT30    = 1 << 30
+	NIL          = -1
+
+	// for dijkstra, prim, and so on
+	WHITE = 0
+	GRAY  = 1
+	BLACK = 2
+)
+
+// Min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func Min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+type Monster struct {
+	key  int
+	x, h int
+}
+type MonsterList []*Monster
+type byKey struct {
+	MonsterList
+}
+
+func (l MonsterList) Len() int {
+	return len(l)
+}
+func (l MonsterList) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l byKey) Less(i, j int) bool {
+	return l.MonsterList[i].key < l.MonsterList[j].key
+}
+
+// how to use
+// L := make(MonsterList, 0, 200000+5)
+// L = append(L, &Monster{key: intValue})
+// sort.Stable(byKey{ L })                // Stable ASC
+// sort.Stable(sort.Reverse(byKey{ L }))  // Stable DESC
+
+var n, d, a int
+var X, H []int
+var L MonsterList
+
+func main() {
+	n, d, a = ReadInt3()
+	X, H = make([]int, n), make([]int, n)
+	for i := 0; i < n; i++ {
+		x, h := ReadInt2()
+		X[i], H[i] = x, h
+	}
+	L = make(MonsterList, 0)
+	for i := 0; i < n; i++ {
+		L = append(L, &Monster{key: X[i], x: X[i], h: H[i]})
+	}
+	sort.Stable(byKey{L})
+
+	f := func(lv, rv T) T {
+		return T(Min(int(lv), int(rv)))
+	}
+	g := func(to T, from E) T {
+		return to + T(from)
+	}
+	h := func(to, from E) E {
+		return to + from
+	}
+	p := func(e E, length int) E {
+		return e
+	}
+	ti := 1 << 40
+	ei := 0
+	lst := NewLazySegmentTree(n, f, g, h, p, T(ti), E(ei))
+	for i := 0; i < n; i++ {
+		lst.Set(i, T(L[i].h))
+	}
+	lst.Build()
+
+	ans := 0
+	for i := 0; i < n; i++ {
+		monH := int(lst.Get(i))
+		x := L[i].x
+		if monH <= 0 {
+			continue
+		}
+
+		// iを左端にしてnum回爆撃
+		num := (monH + (a - 1)) / a
+		ans += num
+		// x+2d以下の座標のモンスターで最も右に居るものを探索
+		mri := sub(x + 2*d)
+		// [x, x+2*d]に対してRAQ
+		lst.Update(i, mri+1, E(-a*num))
+	}
+
+	fmt.Println(ans)
+}
+
+func sub(rx int) int {
+	isOK := func(mid int) bool {
+		x := L[mid].x
+		if x <= rx {
+			return true
+		}
+		return false
+	}
+
+	// ng, ok := -1, len(SliceName)
+	ng, ok := n, 0
+	for int(math.Abs(float64(ok-ng))) > 1 {
+		mid := (ok + ng) / 2
+		if isOK(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// Assumption: T == E
+type T int // (T, f): Monoid
+type E int // (E, h): Operator Monoid
+
+type LazySegmentTree struct {
+	sz   int
+	data []T
+	lazy []E
+	f    func(lv, rv T) T        // T <> T -> T
+	g    func(to T, from E) T    // T <> E -> T (assignment operator)
+	h    func(to, from E) E      // E <> E -> E (assignment operator)
+	p    func(e E, length int) E // E <> N -> E
+	ti   T
+	ei   E
+}
+
+func NewLazySegmentTree(
+	n int,
+	f func(lv, rv T) T, g func(to T, from E) T,
+	h func(to, from E) E, p func(e E, length int) E,
+	ti T, ei E,
+) *LazySegmentTree {
+	lst := new(LazySegmentTree)
+	lst.f, lst.g, lst.h, lst.p = f, g, h, p
+	lst.ti, lst.ei = ti, ei
+
+	lst.sz = 1
+	for lst.sz < n {
+		lst.sz *= 2
+	}
+
+	lst.data = make([]T, 2*lst.sz-1)
+	lst.lazy = make([]E, 2*lst.sz-1)
+	for i := 0; i < 2*lst.sz-1; i++ {
+		lst.data[i] = lst.ti
+		lst.lazy[i] = lst.ei
+	}
+
+	return lst
+}
+
+func (lst *LazySegmentTree) Set(k int, x T) {
+	lst.data[k+(lst.sz-1)] = x
+}
+
+func (lst *LazySegmentTree) Build() {
+	for i := lst.sz - 2; i >= 0; i-- {
+		lst.data[i] = lst.f(lst.data[2*i+1], lst.data[2*i+2])
+	}
+}
+
+func (lst *LazySegmentTree) propagate(k, length int) {
+	if lst.lazy[k] != lst.ei {
+		if k < lst.sz-1 {
+			lst.lazy[2*k+1] = lst.h(lst.lazy[2*k+1], lst.lazy[k])
+			lst.lazy[2*k+2] = lst.h(lst.lazy[2*k+2], lst.lazy[k])
+		}
+		lst.data[k] = lst.g(lst.data[k], lst.p(lst.lazy[k], length))
+		lst.lazy[k] = lst.ei
+	}
+}
+
+func (lst *LazySegmentTree) Update(a, b int, x E) T {
+	return lst.update(a, b, x, 0, 0, lst.sz)
+}
+
+func (lst *LazySegmentTree) update(a, b int, x E, k, l, r int) T {
+	lst.propagate(k, r-l)
+
+	if r <= a || b <= l {
+		return lst.data[k]
+	}
+
+	if a <= l && r <= b {
+		lst.lazy[k] = lst.h(lst.lazy[k], x)
+		lst.propagate(k, r-l)
+		return lst.data[k]
+	}
+
+	lv := lst.update(a, b, x, 2*k+1, l, (l+r)/2)
+	rv := lst.update(a, b, x, 2*k+2, (l+r)/2, r)
+	lst.data[k] = lst.f(lv, rv)
+	return lst.data[k]
+}
+
+func (lst *LazySegmentTree) Query(a, b int) T {
+	return lst.query(a, b, 0, 0, lst.sz)
+}
+
+func (lst *LazySegmentTree) query(a, b, k, l, r int) T {
+	lst.propagate(k, r-l)
+
+	if r <= a || b <= l {
+		return lst.ti
+	}
+
+	if a <= l && r <= b {
+		return lst.data[k]
+	}
+
+	lv := lst.query(a, b, 2*k+1, l, (l+r)/2)
+	rv := lst.query(a, b, 2*k+2, (l+r)/2, r)
+	return lst.f(lv, rv)
+}
+
+func (lst *LazySegmentTree) Get(k int) T {
+	return lst.Query(k, k+1)
+}
+
+/*
+- まずは全探索を検討しましょう
+- MODは最後にとりましたか？
+- ループを抜けた後も処理が必要じゃありませんか？
+- 和・積・あまりを求められたらint64が必要ではありませんか？
+- いきなりオーバーフローはしていませんか？
+- MOD取る系はint64必須ですよ？
+*/
+
+/*******************************************************************/
