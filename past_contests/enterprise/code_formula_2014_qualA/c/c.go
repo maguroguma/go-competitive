9ff436efd48a09ba638b7dee8676c26d455b4321
myokoyama <part7steelballrun.to.8@gmail.com>
2020-03-26 00:55:34 +0900

    solve some past problems

---
+++ past_contests/enterprise/code_formula_2014_qualA/c/c.go
@@ -0,0 +1,425 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+/*********** I/O ***********/
+
+var (
+	// ReadString returns a WORD string.
+	ReadString func() string
+	stdout     *bufio.Writer
+)
+
+func init() {
+	ReadString = newReadString(os.Stdin)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+func newReadString(ior io.Reader) func() string {
+	r := bufio.NewScanner(ior)
+	// r.Buffer(make([]byte, 1024), int(1e+11)) // for AtCoder
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	// Split sets the split function for the Scanner. The default split function is ScanLines.
+	// Split panics if it is called after scanning has started.
+	r.Split(bufio.ScanWords)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// ReadInt returns an integer.
+func ReadInt() int {
+	return int(readInt64())
+}
+func ReadInt2() (int, int) {
+	return int(readInt64()), int(readInt64())
+}
+func ReadInt3() (int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64())
+}
+func ReadInt4() (int, int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64()), int(readInt64())
+}
+
+// ReadInt64 returns as integer as int64.
+func ReadInt64() int64 {
+	return readInt64()
+}
+func ReadInt64_2() (int64, int64) {
+	return readInt64(), readInt64()
+}
+func ReadInt64_3() (int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64()
+}
+func ReadInt64_4() (int64, int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64(), readInt64()
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(ReadString(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// ReadIntSlice returns an integer slice that has n integers.
+func ReadIntSlice(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt()
+	}
+	return b
+}
+
+// ReadInt64Slice returns as int64 slice that has n integers.
+func ReadInt64Slice(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt64()
+	}
+	return b
+}
+
+// ReadFloat64 returns an float64.
+func ReadFloat64() float64 {
+	return float64(readFloat64())
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(ReadString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func ReadFloat64Slice(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadFloat64()
+	}
+	return b
+}
+
+// ReadRuneSlice returns a rune slice.
+func ReadRuneSlice() []rune {
+	return []rune(ReadString())
+}
+
+/*********** Debugging ***********/
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
+
+// Strtoi is a wrapper of strconv.Atoi().
+// If strconv.Atoi() returns an error, Strtoi calls panic.
+func Strtoi(s string) int {
+	if i, err := strconv.Atoi(s); err != nil {
+		panic(errors.New("[argument error]: Strtoi only accepts integer string"))
+	} else {
+		return i
+	}
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintDebug is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func PrintDebug(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := ReadString()
+//i := ReadInt()
+//X := ReadIntSlice(n)
+//S := ReadRuneSlice()
+//a := ReadFloat64()
+//A := ReadFloat64Slice(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*
+ASCII code
+
+ASCII   10進数  ASCII   10進数  ASCII   10進数
+!       33      "       34      #       35
+$       36      %       37      &       38
+'       39      (       40      )       41
+*       42      +       43      ,       44
+-       45      .       46      /       47
+0       48      1       49      2       50
+3       51      4       52      5       53
+6       54      7       55      8       56
+9       57      :       58      ;       59
+<       60      =       61      >       62
+?       63      @       64      A       65
+B       66      C       67      D       68
+E       69      F       70      G       71
+H       72      I       73      J       74
+K       75      L       76      M       77
+N       78      O       79      P       80
+Q       81      R       82      S       83
+T       84      U       85      V       86
+W       87      X       88      Y       89
+Z       90      [       91      \       92
+]       93      ^       94      _       95
+`       96      a       97      b       98
+c       99      d       100     e       101
+f       102     g       103     h       104
+i       105     j       106     k       107
+l       108     m       109     n       110
+o       111     p       112     q       113
+r       114     s       115     t       116
+u       117     v       118     w       119
+x       120     y       121     z       122
+{       123     |       124     }       125
+~       126             127
+*/
+
+/*******************************************************************/
+
+const (
+	// General purpose
+	MOD          = 1000000000 + 7
+	ALPHABET_NUM = 26
+	INF_INT64    = math.MaxInt64
+	INF_BIT60    = 1 << 60
+	INF_INT32    = math.MaxInt32
+	INF_BIT30    = 1 << 30
+	NIL          = -1
+
+	// for dijkstra, prim, and so on
+	WHITE = 0
+	GRAY  = 1
+	BLACK = 2
+)
+
+var (
+	n, k int
+	A    [][]int
+)
+
+type Contestant struct {
+	id    int
+	stand int
+	yosen int
+}
+type ContestantList []*Contestant
+type byKey struct {
+	ContestantList
+}
+
+func (l ContestantList) Len() int {
+	return len(l)
+}
+func (l ContestantList) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l byKey) Less(i, j int) bool {
+	// return l.ContestantList[i].key < l.ContestantList[j].key
+	if l.ContestantList[i].stand < l.ContestantList[j].stand {
+		return true
+	} else if l.ContestantList[i].stand > l.ContestantList[j].stand {
+		return false
+	} else {
+		return l.ContestantList[i].yosen < l.ContestantList[j].yosen
+	}
+}
+
+// how to use
+// L := make(ContestantList, 0, 200000+5)
+// L = append(L, &Contestant{key: intValue})
+// sort.Stable(byKey{ L })                // Stable ASC
+// sort.Stable(sort.Reverse(byKey{ L }))  // Stable DESC
+
+func main() {
+	n, k = ReadInt2()
+	for i := 0; i < n; i++ {
+		row := ReadIntSlice(k)
+		A = append(A, row)
+	}
+
+	memo := map[int]Contestant{}
+	for i := 0; i < n; i++ {
+		for j := 0; j < k; j++ {
+			a := A[i][j]
+			if c, ok := memo[a]; ok {
+				// すでに登録済み
+				if ChMin(&c.stand, j) {
+					// 順位が更新されたら予選IDも更新
+					c.yosen = i
+					memo[a] = c
+				}
+			} else {
+				// 初登録
+				contestant := Contestant{id: a, stand: j, yosen: i}
+				memo[a] = contestant
+			}
+		}
+	}
+
+	// for _, c := range memo {
+	// 	PrintDebug("id: %d, standings: %d, yosenID: %d\n", c.id, c.stand, c.yosen)
+	// }
+
+	L := make(ContestantList, 0)
+	for _, c := range memo {
+		// L = append(L, &c)
+		L = append(L, &Contestant{id: c.id, stand: c.stand, yosen: c.yosen})
+	}
+	sort.Stable(byKey{L})
+
+	for _, c := range L {
+		PrintDebug("id: %d, standings: %d, yosenID: %d\n", c.id, c.stand, c.yosen)
+	}
+
+	top := L[:k]
+	M := make([]ContestantList, n)
+	for _, c := range top {
+		M[c.yosen] = append(M[c.yosen], c)
+	}
+	for i := 0; i < n; i++ {
+		sort.Stable(byKey{M[i]})
+	}
+
+	for i := 0; i < n; i++ {
+		answers := []int{}
+		for _, c := range M[i] {
+			answers = append(answers, c.id)
+		}
+		fmt.Println(PrintIntsLine(answers...))
+	}
+
+	// memo := map[int]bool{}
+	// OUTER:
+	// for j := 0; j < k; j++ {
+	// 	for i := 0; i < n; i++ {
+	// 		a := A[i][j]
+	// 		memo[a] = true
+	// 		if len(memo) >= k {
+	// 			break OUTER
+	// 		}
+	// 	}
+	// }
+
+	// for i := 0; i < n; i++ {
+	// 	answers := []int{}
+	// 	for j := 0; j < k; j++ {
+	// 		a := A[i][j]
+	// 		if _, ok := memo[a]; ok {
+	// 			answers = append(answers, a)
+	// 			delete(memo, a)
+	// 		}
+	// 	}
+
+	// 	sort.Sort(sort.IntSlice(answers))
+	// 	fmt.Println(PrintIntsLine(answers...))
+	// }
+}
+
+// ChMin accepts a pointer of integer and a target value.
+// If target value is SMALLER than the first argument,
+//	then the first argument will be updated by the second argument.
+func ChMin(updatedValue *int, target int) bool {
+	if *updatedValue > target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// ChMax accepts a pointer of integer and a target value.
+// If target value is LARGER than the first argument,
+//	then the first argument will be updated by the second argument.
+func ChMax(updatedValue *int, target int) bool {
+	if *updatedValue < target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+/*
+- まずは全探索を検討しましょう
+- MODは最後にとりましたか？
+- 負のMODはちゃんと関数を使って処理していますか？
+- ループを抜けた後も処理が必要じゃありませんか？
+- 和・積・あまりを求められたらint64が必要ではありませんか？
+- いきなりオーバーフローはしていませんか？
+- MOD取る系はint64必須ですよ？
+- 後ろ・逆・ゴールから考えましたか？
+- 3者のうち真ん中に着目しましたか？
+*/
+
+/*******************************************************************/
