860d5121af7f428924764ec50491601d0d054e3c
myokoyama <part7steelballrun.to.8@gmail.com>
2020-05-02 13:40:52 +0900

    done cr638

---
+++ contests/2020/05/20200501_cr638/c/c.go
@@ -0,0 +1,315 @@
+/*
+URL:
+https://codeforces.com/contest/1348/problem/C
+*/
+
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := ReadString()
+//i := ReadInt()
+//X := ReadIntSlice(n)
+//S := ReadRuneSlice()
+//a := ReadFloat64()
+//A := ReadFloat64Slice(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*******************************************************************/
+
+const (
+	// General purpose
+	MOD          = 1000000000 + 7
+	ALPHABET_NUM = 26
+	INF_INT64    = math.MaxInt64
+	INF_BIT60    = 1 << 60
+	INF_INT32    = math.MaxInt32
+	INF_BIT30    = 1 << 30
+	NIL          = -1
+
+	// for dijkstra, prim, and so on
+	WHITE = 0
+	GRAY  = 1
+	BLACK = 2
+)
+
+func init() {
+	ReadString = newReadString(os.Stdin, bufio.ScanWords)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+var (
+	t int
+)
+
+func main() {
+	t = ReadInt()
+	for i := 0; i < t; i++ {
+		n, k := ReadInt2()
+		S := ReadRuneSlice()
+		solve(n, k, S)
+	}
+}
+
+func solve(n, k int, S []rune) {
+	tmp := Char{}
+	for _, r := range S {
+		tmp = append(tmp, r)
+	}
+	sort.Stable(tmp)
+
+	T := []rune{}
+	for i := 0; i < len(tmp); i++ {
+		T = append(T, rune(tmp[i]))
+	}
+
+	if T[0] != T[k-1] {
+		fmt.Printf("%c\n", T[k-1])
+		return
+	}
+
+	if T[0] == T[n-1] {
+		num := Kiriage(n, k)
+		for i := 0; i < num; i++ {
+			fmt.Printf("%c", T[0])
+		}
+		fmt.Printf("\n")
+		return
+	}
+
+	if T[k] == T[n-1] {
+		num := Kiriage(n-k, k)
+		fmt.Printf("%c", T[0])
+		for i := 0; i < num; i++ {
+			fmt.Printf("%c", T[k])
+		}
+		fmt.Printf("\n")
+		return
+	}
+
+	fmt.Printf("%s\n", string(T[k-1:n]))
+
+	// fmt.Println(string(T[k-1 : n]))
+	// for i := 0; i+k-1 < n; i += k {
+	// 	if T[i] == T[i+k-1] {
+	// 		fmt.Printf("%c", T[i])
+	// 	} else {
+	// 		fmt.Printf("%s\n", string(T[i:n]))
+	// 		return
+	// 	}
+	// }
+	// fmt.Printf("\n")
+}
+
+// Kiriage returns Ceil(a/b)
+// a >= 0, b > 0
+func Kiriage(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+type Char []rune
+
+func (a Char) Len() int           { return len(a) }
+func (a Char) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Char) Less(i, j int) bool { return a[i] < a[j] }
+
+// S := Char{'a', 'b', 'c'}
+// sort.Sort(S) or sort.Sort(sort.Reverse(S))
+
+/*******************************************************************/
+
+/*********** I/O ***********/
+
+var (
+	// ReadString returns a WORD string.
+	ReadString func() string
+	stdout     *bufio.Writer
+)
+
+func newReadString(ior io.Reader, sf bufio.SplitFunc) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	r.Split(sf)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// ReadInt returns an integer.
+func ReadInt() int {
+	return int(readInt64())
+}
+func ReadInt2() (int, int) {
+	return int(readInt64()), int(readInt64())
+}
+func ReadInt3() (int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64())
+}
+func ReadInt4() (int, int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64()), int(readInt64())
+}
+
+// ReadInt64 returns as integer as int64.
+func ReadInt64() int64 {
+	return readInt64()
+}
+func ReadInt64_2() (int64, int64) {
+	return readInt64(), readInt64()
+}
+func ReadInt64_3() (int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64()
+}
+func ReadInt64_4() (int64, int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64(), readInt64()
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(ReadString(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// ReadIntSlice returns an integer slice that has n integers.
+func ReadIntSlice(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt()
+	}
+	return b
+}
+
+// ReadInt64Slice returns as int64 slice that has n integers.
+func ReadInt64Slice(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt64()
+	}
+	return b
+}
+
+// ReadFloat64 returns an float64.
+func ReadFloat64() float64 {
+	return float64(readFloat64())
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(ReadString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func ReadFloat64Slice(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadFloat64()
+	}
+	return b
+}
+
+// ReadRuneSlice returns a rune slice.
+func ReadRuneSlice() []rune {
+	return []rune(ReadString())
+}
+
+/*********** Debugging ***********/
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
+
+// Strtoi is a wrapper of strconv.Atoi().
+// If strconv.Atoi() returns an error, Strtoi calls panic.
+func Strtoi(s string) int {
+	if i, err := strconv.Atoi(s); err != nil {
+		panic(errors.New("[argument error]: Strtoi only accepts integer string"))
+	} else {
+		return i
+	}
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintfDebug is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func PrintfDebug(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+// PrintfBufStdout is function for output strings to buffered os.Stdout.
+// You may have to call stdout.Flush() finally.
+func PrintfBufStdout(format string, a ...interface{}) {
+	fmt.Fprintf(stdout, format, a...)
+}
