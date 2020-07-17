86bc23f9c6781898485513b9337a9a88c06e749a
myokoyama <part7steelballrun.to.8@gmail.com>
2020-07-17 23:05:15 +0900

    solve some past problems

---
+++ past_contests/diffOrder/ARC008-C/main.go
@@ -0,0 +1,390 @@
+/*
+URL:
+https://atcoder.jp/contests/arc008/tasks/arc008_3
+*/
+
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := ReadString()
+//i := ReadInt()
+//X := ReadIntSlice(n)
+//S := ReadRuneSlice()
+//a := ReadFloat64()
+//A := ReadFloat64Slice(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*******************************************************************/
+
+const (
+	// General purpose
+	MOD = 1000000000 + 7
+	// MOD          = 998244353
+	ALPHABET_NUM = 26
+	INF_INT64    = math.MaxInt64
+	INF_BIT60    = 1 << 60
+	INF_INT32    = math.MaxInt32
+	INF_BIT30    = 1 << 30
+	NIL          = -1
+
+	// for dijkstra, prim, and so on
+	WHITE = 0
+	GRAY  = 1
+	BLACK = 2
+)
+
+func init() {
+	// bufio.ScanWords <---> bufio.ScanLines
+	ReadString = newReadString(os.Stdin, bufio.ScanWords)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+func distance(x0, y0, x1, y1 int) float64 {
+	tmp := (x0-x1)*(x0-x1) + (y0-y1)*(y0-y1)
+	return math.Sqrt(float64(tmp))
+}
+
+// Min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func Min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+var (
+	n          int
+	X, Y, T, R []int
+
+	G [1000 + 50][]Edge
+)
+
+func main() {
+	n = ReadInt()
+	for i := 0; i < n; i++ {
+		x, y, t, r := ReadInt4()
+		X = append(X, x)
+		Y = append(Y, y)
+		T = append(T, t)
+		R = append(R, r)
+	}
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			x0, y0, x1, y1 := X[i], Y[i], X[j], Y[j]
+			df := distance(x0, y0, x1, y1)
+
+			t0, r0, t1, r1 := T[i], R[i], T[j], R[j]
+			// i => j
+			vi := float64(Min(t0, r1))
+			G[i] = append(G[i], Edge{to: j, cost: df / vi})
+
+			// j => i
+			vj := float64(Min(t1, r0))
+			G[j] = append(G[j], Edge{to: i, cost: df / vj})
+		}
+	}
+
+	dp, _ := dijkstra(0, n, G[:n])
+	PrintfDebug("dp: %v\n", dp)
+
+	sort.Slice(dp, func(i, j int) bool { return dp[i] > dp[j] })
+	ans := 0.0
+	for i := 0; i < n-1; i++ {
+		plus := float64(i)
+		if ans < dp[i]+plus {
+			ans = dp[i] + plus
+		}
+	}
+	fmt.Println(ans)
+}
+
+type (
+	Edge struct {
+		to   int
+		cost float64
+	}
+	Vertex struct {
+		pri float64
+		id  int
+	}
+)
+
+const INF_DIJK = 1 << 60
+
+func dijkstra(sid, n int, AG [][]Edge) (dp []float64, parents []int) {
+	dp = make([]float64, n)
+	parents = make([]int, n)
+	colors := make([]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = float64(INF_DIJK)
+		colors[i], parents[i] = WHITE, -1
+	}
+
+	temp := make(VertexPQ, 0, 100000+5)
+	pq := &temp
+	heap.Init(pq)
+	heap.Push(pq, &Vertex{pri: 0, id: sid})
+	dp[sid] = 0
+	colors[sid] = GRAY
+
+	for pq.Len() > 0 {
+		pop := heap.Pop(pq).(*Vertex)
+
+		colors[pop.id] = BLACK
+
+		if pop.pri > dp[pop.id] {
+			continue
+		}
+
+		for _, e := range AG[pop.id] {
+			if colors[e.to] == BLACK {
+				continue
+			}
+
+			if dp[e.to] > dp[pop.id]+e.cost {
+				dp[e.to] = dp[pop.id] + e.cost
+				heap.Push(pq, &Vertex{pri: dp[e.to], id: e.to})
+				colors[e.to], parents[e.to] = GRAY, pop.id
+			}
+		}
+	}
+
+	return dp, parents
+}
+
+type VertexPQ []*Vertex
+
+func (pq VertexPQ) Len() int           { return len(pq) }
+func (pq VertexPQ) Less(i, j int) bool { return pq[i].pri < pq[j].pri } // <: ASC, >: DESC
+func (pq VertexPQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+func (pq *VertexPQ) Push(x interface{}) {
+	item := x.(*Vertex)
+	*pq = append(*pq, item)
+}
+func (pq *VertexPQ) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+// how to use
+// temp := make(VertexPQ, 0, 100000+1)
+// pq := &temp
+// heap.Init(pq)
+// heap.Push(pq, &Vertex{pri: intValue})
+// popped := heap.Pop(pq).(*Vertex)
+
+func solve() {
+}
+
+/*******************************************************************/
+
+/*********** I/O ***********/
+
+var (
+	// ReadString returns a WORD string.
+	ReadString func() string
+	stdout     *bufio.Writer
+)
+
+func newReadString(ior io.Reader, sf bufio.SplitFunc) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	r.Split(sf)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// ReadInt returns an integer.
+func ReadInt() int {
+	return int(readInt64())
+}
+func ReadInt2() (int, int) {
+	return int(readInt64()), int(readInt64())
+}
+func ReadInt3() (int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64())
+}
+func ReadInt4() (int, int, int, int) {
+	return int(readInt64()), int(readInt64()), int(readInt64()), int(readInt64())
+}
+
+// ReadInt64 returns as integer as int64.
+func ReadInt64() int64 {
+	return readInt64()
+}
+func ReadInt64_2() (int64, int64) {
+	return readInt64(), readInt64()
+}
+func ReadInt64_3() (int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64()
+}
+func ReadInt64_4() (int64, int64, int64, int64) {
+	return readInt64(), readInt64(), readInt64(), readInt64()
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(ReadString(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// ReadIntSlice returns an integer slice that has n integers.
+func ReadIntSlice(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt()
+	}
+	return b
+}
+
+// ReadInt64Slice returns as int64 slice that has n integers.
+func ReadInt64Slice(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadInt64()
+	}
+	return b
+}
+
+// ReadFloat64 returns an float64.
+func ReadFloat64() float64 {
+	return float64(readFloat64())
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(ReadString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func ReadFloat64Slice(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = ReadFloat64()
+	}
+	return b
+}
+
+// ReadRuneSlice returns a rune slice.
+func ReadRuneSlice() []rune {
+	return []rune(ReadString())
+}
+
+/*********** Debugging ***********/
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
+
+// Strtoi is a wrapper of strconv.Atoi().
+// If strconv.Atoi() returns an error, Strtoi calls panic.
+func Strtoi(s string) int {
+	if i, err := strconv.Atoi(s); err != nil {
+		panic(errors.New("[argument error]: Strtoi only accepts integer string"))
+	} else {
+		return i
+	}
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintfDebug is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func PrintfDebug(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+// PrintfBufStdout is function for output strings to buffered os.Stdout.
+// You may have to call stdout.Flush() finally.
+func PrintfBufStdout(format string, a ...interface{}) {
+	fmt.Fprintf(stdout, format, a...)
+}
