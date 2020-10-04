c4ee502ff761813ec4696ea7e6d4ce9ab52eccfa
myokoyama <part7steelballrun.to.8@gmail.com>
2020-10-04 20:32:30 +0900

    learned segtree by ACLPC

---
+++ sho-jin/ac-library/README.md
@@ -0,0 +1,3 @@
+# AtCoder Libraryの移植
+
+Last Change: 2020-10-04 19:31:22.
---
+++ sho-jin/ac-library/segtree/yc875/main.go
@@ -0,0 +1,486 @@
+/*
+URL:
+https://yukicoder.me/problems/no/875
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+var (
+	n, q int
+	A    []int
+)
+
+func main() {
+	defer stdout.Flush()
+
+	n, q = readi2()
+	A = readis(n)
+
+	v := make([]S, n)
+	for i := 0; i < n; i++ {
+		ob := S{A[i], i + 1}
+		v[i] = ob
+	}
+	e := func() S {
+		return S{INF_B60, -1}
+	}
+	m := func(a, b S) S {
+		if a.val < b.val {
+			return a
+		}
+		return b
+	}
+	seg := NewSegtree(v, e, m)
+
+	for i := 0; i < q; i++ {
+		com := readi()
+		l, r := readi2()
+		l--
+		if com == 1 {
+			ol := seg.Get(l)
+			or := seg.Get(r - 1)
+			ol.idx, or.idx = or.idx, ol.idx
+			seg.Set(l, or)
+			seg.Set(r-1, ol)
+		} else {
+			s := seg.Prod(l, r)
+			printf("%d\n", s.idx)
+		}
+	}
+}
+
+// originated from:
+// https://qiita.com/EmptyBox_0/items/2f8e3cf7bd44e0f789d5#segtree
+type S struct {
+	val, idx int
+}
+
+type E func() S
+type Merger func(a, b S) S
+type Compare func(v S) bool
+type Segtree struct {
+	n      int
+	size   int
+	log    int
+	d      []S
+	e      E
+	merger Merger
+}
+
+func NewSegtree(v []S, e E, m Merger) *Segtree {
+	seg := new(Segtree)
+	seg.n = len(v)
+	seg.log = seg._ceilPow2(seg.n)
+	seg.size = 1 << uint(seg.log)
+	seg.d = make([]S, 2*seg.size)
+	seg.e = e
+	seg.merger = m
+	for i := range seg.d {
+		seg.d[i] = seg.e()
+	}
+	for i := 0; i < seg.n; i++ {
+		seg.d[seg.size+i] = v[i]
+	}
+	for i := seg.size - 1; i >= 1; i-- {
+		seg._update(i)
+	}
+	return seg
+}
+
+func (seg *Segtree) _update(k int) {
+	seg.d[k] = seg.merger(seg.d[2*k], seg.d[2*k+1])
+}
+
+func (seg *Segtree) Set(p int, x S) {
+	p += seg.size
+	seg.d[p] = x
+	for i := 1; i <= seg.log; i++ {
+		seg._update(p >> uint(i))
+	}
+}
+
+func (seg *Segtree) Get(p int) S {
+	return seg.d[p+seg.size]
+}
+
+func (seg *Segtree) Prod(l, r int) S {
+	sml, smr := seg.e(), seg.e()
+	l += seg.size
+	r += seg.size
+	for l < r {
+		if (l & 1) == 1 {
+			sml = seg.merger(sml, seg.d[l])
+			l++
+		}
+		if (r & 1) == 1 {
+			r--
+			smr = seg.merger(seg.d[r], smr)
+		}
+		l >>= 1
+		r >>= 1
+	}
+	return seg.merger(sml, smr)
+}
+
+func (seg *Segtree) AllProd() S {
+	return seg.d[1]
+}
+
+func (seg *Segtree) MaxRight(l int, cmp Compare) int {
+	if l == seg.n {
+		return seg.n
+	}
+	l += seg.size
+	sm := seg.e()
+	for {
+		for l%2 == 0 {
+			l >>= 1
+		}
+		if !cmp(seg.merger(sm, seg.d[l])) {
+			for l < seg.size {
+				l = 2 * l
+				if cmp(seg.merger(sm, seg.d[l])) {
+					sm = seg.merger(sm, seg.d[l])
+					l++
+				}
+			}
+			return l - seg.size
+		}
+		sm = seg.merger(sm, seg.d[l])
+		l++
+		if l&-l == l {
+			break
+		}
+	}
+	return seg.n
+}
+
+func (seg *Segtree) MinLeft(r int, cmp Compare) int {
+	if r == 0 {
+		return 0
+	}
+	r += seg.size
+	sm := seg.e()
+	for {
+		r--
+		for r > 1 && r%2 != 0 {
+			r >>= 1
+		}
+		if !cmp(seg.merger(seg.d[r], sm)) {
+			for r < seg.size {
+				r = 2*r + 1
+				if cmp(seg.merger(seg.d[r], sm)) {
+					sm = seg.merger(seg.d[r], sm)
+					r--
+				}
+			}
+			return r + 1 - seg.size
+		}
+		sm = seg.merger(seg.d[r], sm)
+		if r&-r == r {
+			break
+		}
+	}
+	return 0
+}
+
+func (seg *Segtree) _ceilPow2(n int) int {
+	x := 0
+	for (1 << uint(x)) < n {
+		x++
+	}
+	return x
+}
+
+/*******************************************************************/
+
+/********** common constants **********/
+
+const (
+	MOD = 1000000000 + 7
+	// MOD          = 998244353
+	ALPH_N  = 26
+	INF_I64 = math.MaxInt64
+	INF_B60 = 1 << 60
+	INF_I32 = math.MaxInt32
+	INF_B30 = 1 << 30
+	NIL     = -1
+	EPS     = 1e-10
+)
+
+/********** bufio setting **********/
+
+func init() {
+	// bufio.ScanWords <---> bufio.ScanLines
+	reads = newReadString(os.Stdin, bufio.ScanWords)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+// mod can calculate a right residual whether value is positive or negative.
+func mod(val, m int) int {
+	res := val % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+// min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// max returns the max integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// chmin accepts a pointer of integer and a target value.
+// If target value is SMALLER than the first argument,
+//	then the first argument will be updated by the second argument.
+func chmin(updatedValue *int, target int) bool {
+	if *updatedValue > target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// chmax accepts a pointer of integer and a target value.
+// If target value is LARGER than the first argument,
+//	then the first argument will be updated by the second argument.
+func chmax(updatedValue *int, target int) bool {
+	if *updatedValue < target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// sum returns multiple integers sum.
+func sum(integers ...int) int {
+	var s int
+	s = 0
+
+	for _, i := range integers {
+		s += i
+	}
+
+	return s
+}
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := reads()
+//i := readi()
+//X := readis(n)
+//S := readrs()
+//a := readf()
+//A := readfs(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*********** Input ***********/
+
+var (
+	// reads returns a WORD string.
+	reads  func() string
+	stdout *bufio.Writer
+)
+
+func newReadString(ior io.Reader, sf bufio.SplitFunc) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	r.Split(sf)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// readi returns an integer.
+func readi() int {
+	return int(_readInt64())
+}
+func readi2() (int, int) {
+	return int(_readInt64()), int(_readInt64())
+}
+func readi3() (int, int, int) {
+	return int(_readInt64()), int(_readInt64()), int(_readInt64())
+}
+func readi4() (int, int, int, int) {
+	return int(_readInt64()), int(_readInt64()), int(_readInt64()), int(_readInt64())
+}
+
+// readll returns as integer as int64.
+func readll() int64 {
+	return _readInt64()
+}
+func readll2() (int64, int64) {
+	return _readInt64(), _readInt64()
+}
+func readll3() (int64, int64, int64) {
+	return _readInt64(), _readInt64(), _readInt64()
+}
+func readll4() (int64, int64, int64, int64) {
+	return _readInt64(), _readInt64(), _readInt64(), _readInt64()
+}
+
+func _readInt64() int64 {
+	i, err := strconv.ParseInt(reads(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// readis returns an integer slice that has n integers.
+func readis(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = readi()
+	}
+	return b
+}
+
+// readlls returns as int64 slice that has n integers.
+func readlls(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = readll()
+	}
+	return b
+}
+
+// readf returns an float64.
+func readf() float64 {
+	return float64(_readFloat64())
+}
+
+func _readFloat64() float64 {
+	f, err := strconv.ParseFloat(reads(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func readfs(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = readf()
+	}
+	return b
+}
+
+// readrs returns a rune slice.
+func readrs() []rune {
+	return []rune(reads())
+}
+
+/*********** Output ***********/
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// Printf is function for output strings to buffered os.Stdout.
+// You may have to call stdout.Flush() finally.
+func printf(format string, a ...interface{}) {
+	fmt.Fprintf(stdout, format, a...)
+}
+
+/*********** Debugging ***********/
+
+// debugf is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func debugf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
