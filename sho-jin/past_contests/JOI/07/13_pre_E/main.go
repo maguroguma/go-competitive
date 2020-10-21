b5ca8c534a2e40e5bf6003a2468b918ec7991fb0
myokoyama <part7steelballrun.to.8@gmail.com>
2020-10-22 00:19:01 +0900

    solve some past problems

---
+++ sho-jin/past_contests/JOI/07/13_pre_E/main.go
@@ -0,0 +1,419 @@
+/*
+URL:
+https://atcoder.jp/contests/joi2013yo/tasks/joi2013yo_e
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var (
+	n, k int
+
+	X, Y, Z    []int
+	A, B, C    []int
+	SX, SY, SZ []int
+
+	// V [200][200][200]int
+	V [400][400][400]int
+)
+
+func main() {
+	defer stdout.Flush()
+
+	n, k = readi2()
+	X, Y, Z = make([]int, n), make([]int, n), make([]int, n)
+	A, B, C = make([]int, n), make([]int, n), make([]int, n)
+	for i := 0; i < n; i++ {
+		x, y, z := readi3()
+		X[i], Y[i], Z[i] = x, y, z
+		a, b, c := readi3()
+		A[i], B[i], C[i] = a, b, c
+
+		SX = append(SX, x, a)
+		SY = append(SY, y, b)
+		SZ = append(SZ, z, c)
+
+		// なぜか↓では駄目だった
+		// SX = append(SX, x-1, x, x+1, a-1, a, a+1)
+		// SY = append(SY, y-1, y, y+1, b-1, b, b+1)
+		// SZ = append(SZ, z-1, z, z+1, c-1, c, c+1)
+	}
+
+	_, tox, invx := ZaAtsu1Dim(SX, 0)
+	_, toy, invy := ZaAtsu1Dim(SY, 0)
+	_, toz, invz := ZaAtsu1Dim(SZ, 0)
+
+	for idx := 0; idx < n; idx++ {
+		x, y, z, a, b, c := X[idx], Y[idx], Z[idx], A[idx], B[idx], C[idx]
+		xs := tox[x]
+		xt := tox[a]
+		ys := toy[y]
+		yt := toy[b]
+		zs := toz[z]
+		zt := toz[c]
+
+		for i := xs; i < xt; i++ {
+			for j := ys; j < yt; j++ {
+				for l := zs; l < zt; l++ {
+					V[i][j][l]++
+				}
+			}
+		}
+	}
+
+	ans := 0
+	for i := 0; i < 200; i++ {
+		for j := 0; j < 200; j++ {
+			for l := 0; l < 200; l++ {
+				if V[i][j][l] >= k {
+					xs := invx[i]
+					xt := invx[i+1]
+					ys := invy[j]
+					yt := invy[j+1]
+					zs := invz[l]
+					zt := invz[l+1]
+
+					ans += (xt - xs) * (yt - ys) * (zt - zs)
+				}
+			}
+		}
+	}
+
+	fmt.Println(ans)
+}
+
+// ZaAtsu1Dim returns 3 values.
+// pressed: pressed slice of the original slice
+// orgToPress: map for translating original value to pressed value
+// pressToOrg: reverse resolution of orgToPress
+// O(nlogn)
+func ZaAtsu1Dim(org []int, initVal int) (pressed []int, orgToPress, pressToOrg map[int]int) {
+	pressed = make([]int, len(org))
+	copy(pressed, org)
+	sort.Sort(sort.IntSlice(pressed))
+
+	orgToPress = make(map[int]int)
+	for i := 0; i < len(org); i++ {
+		if i == 0 {
+			orgToPress[pressed[0]] = initVal
+			continue
+		}
+
+		if pressed[i-1] != pressed[i] {
+			initVal++
+			orgToPress[pressed[i]] = initVal
+		}
+	}
+
+	for i := 0; i < len(org); i++ {
+		pressed[i] = orgToPress[org[i]]
+	}
+
+	pressToOrg = make(map[int]int)
+	for k, v := range orgToPress {
+		pressToOrg[v] = k
+	}
+
+	return
+}
+
+/*******************************************************************/
+
+/********** common constants **********/
+
+const (
+	MOD = 1000000000 + 7
+	// MOD          = 998244353
+	ALPH_N  = 26
+	INF_I64 = math.MaxInt64
+	INF_B60 = 1 << 60
+	INF_I32 = math.MaxInt32
+	INF_B30 = 1 << 30
+	NIL     = -1
+	EPS     = 1e-10
+)
+
+/********** bufio setting **********/
+
+func init() {
+	// bufio.ScanWords <---> bufio.ScanLines
+	reads = newReadString(os.Stdin, bufio.ScanWords)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+// mod can calculate a right residual whether value is positive or negative.
+func mod(val, m int) int {
+	res := val % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+// min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// max returns the max integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// chmin accepts a pointer of integer and a target value.
+// If target value is SMALLER than the first argument,
+//	then the first argument will be updated by the second argument.
+func chmin(updatedValue *int, target int) bool {
+	if *updatedValue > target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// chmax accepts a pointer of integer and a target value.
+// If target value is LARGER than the first argument,
+//	then the first argument will be updated by the second argument.
+func chmax(updatedValue *int, target int) bool {
+	if *updatedValue < target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// sum returns multiple integers sum.
+func sum(integers ...int) int {
+	var s int
+	s = 0
+
+	for _, i := range integers {
+		s += i
+	}
+
+	return s
+}
+
+// abs is integer version of math.Abs
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := reads()
+//i := readi()
+//X := readis(n)
+//S := readrs()
+//a := readf()
+//A := readfs(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*********** Input ***********/
+
+var (
+	// reads returns a WORD string.
+	reads  func() string
+	stdout *bufio.Writer
+)
+
+func newReadString(ior io.Reader, sf bufio.SplitFunc) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	r.Split(sf)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// readi returns an integer.
+func readi() int {
+	return int(_readInt64())
+}
+func readi2() (int, int) {
+	return int(_readInt64()), int(_readInt64())
+}
+func readi3() (int, int, int) {
+	return int(_readInt64()), int(_readInt64()), int(_readInt64())
+}
+func readi4() (int, int, int, int) {
+	return int(_readInt64()), int(_readInt64()), int(_readInt64()), int(_readInt64())
+}
+
+// readll returns as integer as int64.
+func readll() int64 {
+	return _readInt64()
+}
+func readll2() (int64, int64) {
+	return _readInt64(), _readInt64()
+}
+func readll3() (int64, int64, int64) {
+	return _readInt64(), _readInt64(), _readInt64()
+}
+func readll4() (int64, int64, int64, int64) {
+	return _readInt64(), _readInt64(), _readInt64(), _readInt64()
+}
+
+func _readInt64() int64 {
+	i, err := strconv.ParseInt(reads(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// readis returns an integer slice that has n integers.
+func readis(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = readi()
+	}
+	return b
+}
+
+// readlls returns as int64 slice that has n integers.
+func readlls(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = readll()
+	}
+	return b
+}
+
+// readf returns an float64.
+func readf() float64 {
+	return float64(_readFloat64())
+}
+
+func _readFloat64() float64 {
+	f, err := strconv.ParseFloat(reads(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func readfs(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = readf()
+	}
+	return b
+}
+
+// readrs returns a rune slice.
+func readrs() []rune {
+	return []rune(reads())
+}
+
+/*********** Output ***********/
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// Printf is function for output strings to buffered os.Stdout.
+// You may have to call stdout.Flush() finally.
+func printf(format string, a ...interface{}) {
+	fmt.Fprintf(stdout, format, a...)
+}
+
+/*********** Debugging ***********/
+
+// debugf is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func debugf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
