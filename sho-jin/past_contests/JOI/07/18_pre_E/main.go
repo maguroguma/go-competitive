622a27544932d367fed70e0e4e467a02baef7719
myokoyama <part7steelballrun.to.8@gmail.com>
2020-10-29 00:52:21 +0900

    solve some past problems

---
+++ sho-jin/past_contests/JOI/07/18_pre_E/main.go
@@ -0,0 +1,563 @@
+/*
+URL:
+https://atcoder.jp/contests/joi2018yo/tasks/joi2018_yo_e
+*/
+
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+var (
+	h, w int
+	A    [][]int
+
+	G [][]Edge
+)
+
+const (
+	D_MAX = 1000
+)
+
+func toid(i, j int) int { return w*i + j }
+
+func togid(id, L int) int { return w*h*L + id }
+func toL(gid int) int     { return gid / (w * h) }
+func tonid(gid int) int   { return gid % (w * h) }
+func toy(gid int) int     { return tonid(gid) / w }
+func tox(gid int) int     { return tonid(gid) % w }
+
+// toy := func(id int) int { return id / w }
+// tox := func(id int) int { return id % w }
+
+func main() {
+	defer stdout.Flush()
+
+	h, w = readi2()
+	for i := 0; i < h; i++ {
+		row := readis(w)
+		A = append(A, row)
+	}
+
+	steps := [4][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+
+	N := h * w * D_MAX
+	G = make([][]Edge, N)
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			cid := toid(i, j)
+
+			for _, s := range steps {
+				dy, dx := s[0], s[1]
+				ny, nx := i+dy, j+dx
+				if 0 <= ny && ny < h && 0 <= nx && nx < w {
+					nid := toid(ny, nx)
+
+					G[cid] = append(G[cid], Edge{to: nid, w: Weight{1}})
+				}
+			}
+		}
+	}
+
+	vinf := Value{INF_B30}
+	less := func(l, r Value) bool { return l.v < r.v }
+	estimate := func(cid int, cv Value, e Edge) Value { return Value{cv.v + e.w.v} }
+	SS := []StartPoint{{id: togid(toid(0, 0), 0), vzero: Value{0}}}
+
+	ds := NewDijkstraSolver(vinf, less, estimate)
+	dp := ds.Dijkstra(SS, N, G)
+
+	ans := INF_B30
+	for L := 0; L <= 950; L++ {
+		chmin(&ans, dp[togid(toid(h-1, w-1), L)].v)
+	}
+
+	fmt.Println(ans)
+}
+
+// type Value and Weight should be modified according to problems.
+
+// DP value type
+type Value struct {
+	v int
+}
+
+// weight of edge
+type Weight struct {
+	v int
+}
+
+// edge of graph
+type Edge struct {
+	to int
+	w  Weight
+}
+
+// for initializing start points of dijkstra algorithm
+type StartPoint struct {
+	id    int
+	vzero Value
+}
+
+// Less returns l < r, and shared with pq.
+type Less func(l, r Value) bool
+
+// Estimate returns next value considered by transition.
+type Estimate func(cid int, cv Value, e Edge) Value
+
+func NewDijkstraSolver(vinf Value, less Less, estimate Estimate) *DijkstraSolver {
+	ds := new(DijkstraSolver)
+
+	// shared with priority queue
+	__less = less
+
+	ds.vinf, ds.less, ds.estimate = vinf, less, estimate
+
+	return ds
+}
+
+// verified by [ABC143-E](https://atcoder.jp/contests/abc143/tasks/abc143_e)
+func (ds *DijkstraSolver) Dijkstra(S []StartPoint, n int, AG [][]Edge) []Value {
+	// initialize data
+	dp, colors := ds._initAll(n)
+
+	// configure about start points (some problems have multi start points)
+	pq := ds._initStartPoint(S, dp, colors)
+
+	// body of dijkstra algorithm
+	for pq.Len() > 0 {
+		pop := pq.pop()
+		colors[pop.id] = BLACK
+		if ds.less(dp[pop.id], pop.v) {
+			continue
+		}
+
+		// to next node
+		cid := tonid(pop.id)
+		for _, e := range AG[cid] {
+			if toL(pop.id)+1 >= D_MAX {
+				continue
+			}
+
+			nid := togid(e.to, toL(pop.id)+1)
+
+			if colors[nid] == BLACK {
+				continue
+			}
+
+			// update optimal value of the next node
+			// nv := ds.estimate(cid, pop.v, e)
+			y := toy(nid)
+			x := tox(nid)
+			nv := Value{pop.v.v + (toL(nid)*2-1)*A[y][x]}
+
+			if ds.less(nv, dp[nid]) {
+				dp[nid] = nv
+				pq.push(&Vertex{id: nid, v: nv})
+				colors[nid] = GRAY
+			}
+		}
+	}
+
+	return dp
+}
+
+type DijkstraSolver struct {
+	vinf     Value
+	less     Less
+	estimate Estimate
+}
+
+const (
+	WHITE = 0
+	GRAY  = 1
+	BLACK = 2
+)
+
+// _initAll returns initialized dp and colors slices.
+func (ds *DijkstraSolver) _initAll(n int) (dp []Value, colors []int) {
+	dp, colors = make([]Value, n), make([]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = ds.vinf
+		colors[i] = WHITE
+	}
+
+	return dp, colors
+}
+
+// _initStartPoint returns initialized priority queue, and update dp and colors slices.
+// *This function update arguments (side effects).*
+func (ds *DijkstraSolver) _initStartPoint(S []StartPoint, dp []Value, colors []int) *VertexPQ {
+	pq := NewVertexPQ()
+
+	for _, sp := range S {
+		pq.push(&Vertex{id: sp.id, v: sp.vzero})
+		dp[sp.id] = sp.vzero
+		colors[sp.id] = GRAY
+	}
+
+	return pq
+}
+
+// Less function is shared with a priority queue.
+var __less Less
+
+// Definitions of a priority queue
+type Vertex struct {
+	id int
+	v  Value
+}
+type VertexPQ []*Vertex
+
+func NewVertexPQ() *VertexPQ {
+	temp := make(VertexPQ, 0)
+	pq := &temp
+	heap.Init(pq)
+
+	return pq
+}
+func (pq *VertexPQ) push(target *Vertex) {
+	heap.Push(pq, target)
+}
+func (pq *VertexPQ) pop() *Vertex {
+	return heap.Pop(pq).(*Vertex)
+}
+
+func (pq VertexPQ) Len() int { return len(pq) }
+func (pq VertexPQ) Less(i, j int) bool {
+	return __less(pq[i].v, pq[j].v)
+}
+func (pq VertexPQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+func (pq *VertexPQ) Push(x interface{}) {
+	item := x.(*Vertex)
+	*pq = append(*pq, item)
+}
+func (pq *VertexPQ) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+/*******************************************************************/
+
+/********** common constants **********/
+
+const (
+	MOD = 1000000000 + 7
+	// MOD          = 998244353
+	ALPH_N  = 26
+	INF_I64 = math.MaxInt64
+	INF_B60 = 1 << 60
+	INF_I32 = math.MaxInt32
+	INF_B30 = 1 << 30
+	NIL     = -1
+	EPS     = 1e-10
+)
+
+/********** bufio setting **********/
+
+func init() {
+	// bufio.ScanWords <---> bufio.ScanLines
+	reads = newReadString(os.Stdin, bufio.ScanWords)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+// mod can calculate a right residual whether value is positive or negative.
+func mod(val, m int) int {
+	res := val % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+// min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// max returns the max integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// chmin accepts a pointer of integer and a target value.
+// If target value is SMALLER than the first argument,
+//	then the first argument will be updated by the second argument.
+func chmin(updatedValue *int, target int) bool {
+	if *updatedValue > target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// chmax accepts a pointer of integer and a target value.
+// If target value is LARGER than the first argument,
+//	then the first argument will be updated by the second argument.
+func chmax(updatedValue *int, target int) bool {
+	if *updatedValue < target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// sum returns multiple integers sum.
+func sum(integers ...int) int {
+	var s int
+	s = 0
+
+	for _, i := range integers {
+		s += i
+	}
+
+	return s
+}
+
+// abs is integer version of math.Abs
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+// pow is integer version of math.Pow
+// pow calculate a power by Binary Power (二分累乗法(O(log e))).
+func pow(a, e int) int {
+	if a < 0 || e < 0 {
+		panic(errors.New("[argument error]: PowInt does not accept negative integers"))
+	}
+
+	if e == 0 {
+		return 1
+	}
+
+	if e%2 == 0 {
+		halfE := e / 2
+		half := pow(a, halfE)
+		return half * half
+	}
+
+	return a * pow(a, e-1)
+}
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := reads()
+//i := readi()
+//X := readis(n)
+//S := readrs()
+//a := readf()
+//A := readfs(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*********** Input ***********/
+
+var (
+	// reads returns a WORD string.
+	reads  func() string
+	stdout *bufio.Writer
+)
+
+func newReadString(ior io.Reader, sf bufio.SplitFunc) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	r.Split(sf)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// readi returns an integer.
+func readi() int {
+	return int(_readInt64())
+}
+func readi2() (int, int) {
+	return int(_readInt64()), int(_readInt64())
+}
+func readi3() (int, int, int) {
+	return int(_readInt64()), int(_readInt64()), int(_readInt64())
+}
+func readi4() (int, int, int, int) {
+	return int(_readInt64()), int(_readInt64()), int(_readInt64()), int(_readInt64())
+}
+
+// readll returns as integer as int64.
+func readll() int64 {
+	return _readInt64()
+}
+func readll2() (int64, int64) {
+	return _readInt64(), _readInt64()
+}
+func readll3() (int64, int64, int64) {
+	return _readInt64(), _readInt64(), _readInt64()
+}
+func readll4() (int64, int64, int64, int64) {
+	return _readInt64(), _readInt64(), _readInt64(), _readInt64()
+}
+
+func _readInt64() int64 {
+	i, err := strconv.ParseInt(reads(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// readis returns an integer slice that has n integers.
+func readis(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = readi()
+	}
+	return b
+}
+
+// readlls returns as int64 slice that has n integers.
+func readlls(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = readll()
+	}
+	return b
+}
+
+// readf returns an float64.
+func readf() float64 {
+	return float64(_readFloat64())
+}
+
+func _readFloat64() float64 {
+	f, err := strconv.ParseFloat(reads(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func readfs(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = readf()
+	}
+	return b
+}
+
+// readrs returns a rune slice.
+func readrs() []rune {
+	return []rune(reads())
+}
+
+/*********** Output ***********/
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// Printf is function for output strings to buffered os.Stdout.
+// You may have to call stdout.Flush() finally.
+func printf(format string, a ...interface{}) {
+	fmt.Fprintf(stdout, format, a...)
+}
+
+/*********** Debugging ***********/
+
+// debugf is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func debugf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
