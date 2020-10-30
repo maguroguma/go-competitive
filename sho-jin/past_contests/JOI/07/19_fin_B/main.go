06b414b5a9ae737b355f996512dcc0e809d4403d
myokoyama <part7steelballrun.to.8@gmail.com>
2020-10-31 01:21:32 +0900

    solve some past problems

---
+++ sho-jin/past_contests/JOI/07/19_fin_B/main.go
@@ -0,0 +1,456 @@
+/*
+URL:
+https://atcoder.jp/contests/joi2019ho/tasks/joi2019ho_b
+*/
+
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var (
+	n, m int
+	C    []int
+
+	P []Pict
+)
+
+func main() {
+	defer stdout.Flush()
+
+	n, m = readi2()
+	P = make([]Pict, n)
+	for i := 0; i < n; i++ {
+		s, v := readi2()
+		P[i] = Pict{s, v}
+	}
+	C = readis(m)
+
+	sort.Sort(sort.IntSlice(C))
+
+	sort.Slice(P, func(i, j int) bool {
+		if P[i].v < P[j].v {
+			return true
+		} else if P[i].v > P[j].v {
+			return false
+		} else {
+			return P[i].s < P[j].s
+		}
+	})
+
+	A := []int{}
+	curIdx := n - 1
+	for i := m - 1; i >= 0; i-- {
+		c := C[i]
+		for curIdx >= 0 && P[curIdx].s > c {
+			curIdx--
+		}
+
+		if curIdx >= 0 {
+			A = append(A, curIdx)
+			curIdx--
+		}
+	}
+	fmt.Println(len(A))
+
+	// A := []int{}
+	// mini := -1
+	// for i := 0; i < n; i++ {
+	// 	ok := BinarySearch(m, mini, func(mid int) bool {
+	// 		return C[mid] >= P[i].s
+	// 	})
+	// 	if ok == m {
+	// 		continue
+	// 	}
+	// 	A = append(A, ok)
+	// }
+	// debugf("A: %v\n", A)
+
+	// fmt.Println(LIS(A))
+}
+
+// LIS returns a length of Longest Increasing Subsequence of the argument slice A.
+// O( Nlog(N) )
+func LIS(A []int) int {
+	// data structure
+	const (
+		INIT_VAL = 1 << 60
+		MAX_N    = 200000 + 50
+	)
+	var dp = make([]int, MAX_N)
+
+	// binary search sub function
+	sub := func(a int) int {
+		isOK := func(m, a int) bool {
+			if dp[m] < a {
+				return true
+			}
+			return false
+		}
+		ng, ok := len(dp), -1
+		for int(math.Abs(float64(ok-ng))) > 1 {
+			mid := (ok + ng) / 2
+			if isOK(mid, a) {
+				ok = mid
+			} else {
+				ng = mid
+			}
+		}
+		bIdx := ok
+		return bIdx
+	}
+
+	// main algorithm
+	for i := 0; i < len(dp); i++ {
+		dp[i] = INIT_VAL
+	}
+	for i := 0; i < len(A); i++ {
+		// idx := sub(A[i])
+		// dp[idx+1] = A[i]
+		for v := A[i]; v < m; v++ {
+			idx := sub(v)
+			dp[idx+1] = v
+		}
+	}
+	debugf("dp: %v\n", dp[:m])
+	return sub(INIT_VAL) + 1
+}
+
+func BinarySearch(initOK, initNG int, isOK func(mid int) bool) (ok int) {
+	ng := initNG
+	ok = initOK
+	for int(math.Abs(float64(ok-ng))) > 1 {
+		mid := (ok + ng) / 2
+		if isOK(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+type Pict struct {
+	s, v int
+}
+
+/*******************************************************************/
+
+/********** common constants **********/
+
+const (
+	MOD = 1000000000 + 7
+	// MOD          = 998244353
+	ALPH_N  = 26
+	INF_I64 = math.MaxInt64
+	INF_B60 = 1 << 60
+	INF_I32 = math.MaxInt32
+	INF_B30 = 1 << 30
+	NIL     = -1
+	EPS     = 1e-10
+)
+
+/********** bufio setting **********/
+
+func init() {
+	// bufio.ScanWords <---> bufio.ScanLines
+	reads = newReadString(os.Stdin, bufio.ScanWords)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+// mod can calculate a right residual whether value is positive or negative.
+func mod(val, m int) int {
+	res := val % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+// min returns the min integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// max returns the max integer among input set.
+// This function needs at least 1 argument (no argument causes panic).
+func max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+// chmin accepts a pointer of integer and a target value.
+// If target value is SMALLER than the first argument,
+//	then the first argument will be updated by the second argument.
+func chmin(updatedValue *int, target int) bool {
+	if *updatedValue > target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// chmax accepts a pointer of integer and a target value.
+// If target value is LARGER than the first argument,
+//	then the first argument will be updated by the second argument.
+func chmax(updatedValue *int, target int) bool {
+	if *updatedValue < target {
+		*updatedValue = target
+		return true
+	}
+	return false
+}
+
+// sum returns multiple integers sum.
+func sum(integers ...int) int {
+	var s int
+	s = 0
+
+	for _, i := range integers {
+		s += i
+	}
+
+	return s
+}
+
+// abs is integer version of math.Abs
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+// pow is integer version of math.Pow
+// pow calculate a power by Binary Power (二分累乗法(O(log e))).
+func pow(a, e int) int {
+	if a < 0 || e < 0 {
+		panic(errors.New("[argument error]: PowInt does not accept negative integers"))
+	}
+
+	if e == 0 {
+		return 1
+	}
+
+	if e%2 == 0 {
+		halfE := e / 2
+		half := pow(a, halfE)
+		return half * half
+	}
+
+	return a * pow(a, e-1)
+}
+
+/********** FAU standard libraries **********/
+
+//fmt.Sprintf("%b\n", 255) 	// binary expression
+
+/********** I/O usage **********/
+
+//str := reads()
+//i := readi()
+//X := readis(n)
+//S := readrs()
+//a := readf()
+//A := readfs(n)
+
+//str := ZeroPaddingRuneSlice(num, 32)
+//str := PrintIntsLine(X...)
+
+/*********** Input ***********/
+
+var (
+	// reads returns a WORD string.
+	reads  func() string
+	stdout *bufio.Writer
+)
+
+func newReadString(ior io.Reader, sf bufio.SplitFunc) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	r.Split(sf)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+// readi returns an integer.
+func readi() int {
+	return int(_readInt64())
+}
+func readi2() (int, int) {
+	return int(_readInt64()), int(_readInt64())
+}
+func readi3() (int, int, int) {
+	return int(_readInt64()), int(_readInt64()), int(_readInt64())
+}
+func readi4() (int, int, int, int) {
+	return int(_readInt64()), int(_readInt64()), int(_readInt64()), int(_readInt64())
+}
+
+// readll returns as integer as int64.
+func readll() int64 {
+	return _readInt64()
+}
+func readll2() (int64, int64) {
+	return _readInt64(), _readInt64()
+}
+func readll3() (int64, int64, int64) {
+	return _readInt64(), _readInt64(), _readInt64()
+}
+func readll4() (int64, int64, int64, int64) {
+	return _readInt64(), _readInt64(), _readInt64(), _readInt64()
+}
+
+func _readInt64() int64 {
+	i, err := strconv.ParseInt(reads(), 0, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+// readis returns an integer slice that has n integers.
+func readis(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = readi()
+	}
+	return b
+}
+
+// readlls returns as int64 slice that has n integers.
+func readlls(n int) []int64 {
+	b := make([]int64, n)
+	for i := 0; i < n; i++ {
+		b[i] = readll()
+	}
+	return b
+}
+
+// readf returns an float64.
+func readf() float64 {
+	return float64(_readFloat64())
+}
+
+func _readFloat64() float64 {
+	f, err := strconv.ParseFloat(reads(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+// ReadFloatSlice returns an float64 slice that has n float64.
+func readfs(n int) []float64 {
+	b := make([]float64, n)
+	for i := 0; i < n; i++ {
+		b[i] = readf()
+	}
+	return b
+}
+
+// readrs returns a rune slice.
+func readrs() []rune {
+	return []rune(reads())
+}
+
+/*********** Output ***********/
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintIntsLine(A ...int) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.Itoa(A[i])
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// PrintIntsLine returns integers string delimited by a space.
+func PrintInts64Line(A ...int64) string {
+	res := []rune{}
+
+	for i := 0; i < len(A); i++ {
+		str := strconv.FormatInt(A[i], 10) // 64bit int version
+		res = append(res, []rune(str)...)
+
+		if i != len(A)-1 {
+			res = append(res, ' ')
+		}
+	}
+
+	return string(res)
+}
+
+// Printf is function for output strings to buffered os.Stdout.
+// You may have to call stdout.Flush() finally.
+func printf(format string, a ...interface{}) {
+	fmt.Fprintf(stdout, format, a...)
+}
+
+/*********** Debugging ***********/
+
+// debugf is wrapper of fmt.Fprintf(os.Stderr, format, a...)
+func debugf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+// ZeroPaddingRuneSlice returns binary expressions of integer n with zero padding.
+// For debugging use.
+func ZeroPaddingRuneSlice(n, digitsNum int) []rune {
+	sn := fmt.Sprintf("%b", n)
+
+	residualLength := digitsNum - len(sn)
+	if residualLength <= 0 {
+		return []rune(sn)
+	}
+
+	zeros := make([]rune, residualLength)
+	for i := 0; i < len(zeros); i++ {
+		zeros[i] = '0'
+	}
+
+	res := []rune{}
+	res = append(res, zeros...)
+	res = append(res, []rune(sn)...)
+
+	return res
+}
