186c204378e5cb55ef91ff4f8cb111c1c98dc807
myokoyama <part7steelballrun.to.8@gmail.com>
2021-02-20 20:26:46 +0900

    update bit library and snippet a little

--- lib/bit/bruteforce.go
+++ lib/bit/bruteforce.go
@@ -3,21 +3,21 @@ package bit
 // originated from ktateish@AtCoder
 // e.g.: https://atcoder.jp/contests/abc167/submissions/13042361
 
-// BruteForceByBits01(n, fn) calls fn with []int for each n-bit 0/1 pattern
-// e.g.
-// BruteForceByBits01(10, func(b []int) { fmt.Println(b) }
-// -> [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-//    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-//    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
-//    ...
-func BruteForceByBits01(n int, fn func([]int)) {
-	N := 1 << uint(n)
-	a := make([]int, n)
+// BruteForceByBits01(bitsNum, fn) calls fn with []int for each n-bit 0/1 pattern
+func BruteForceByBits01(bitsNum int, fn func(bits []int)) {
+	// e.g.
+	// BruteForceByBits01(10, func(b []int) { fmt.Println(b) }
+	// -> [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	//    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	//    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+	//    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+	//    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+	//    ...
+	N := 1 << uint(bitsNum)
+	a := make([]int, bitsNum)
 	for i := 0; i < N; i++ {
-		for j := 0; j < n; j++ {
-			k := n - j - 1
+		for j := 0; j < bitsNum; j++ {
+			k := bitsNum - j - 1
 			if i&(1<<uint(j)) == 0 {
 				a[k] = 0
 			} else {
@@ -28,19 +28,19 @@ func BruteForceByBits01(n int, fn func([]int)) {
 	}
 }
 
-// BruteForceByBitsTF(n, fn) calls fn with []bool for each n-bit true/false pattern
-// e.g.
-// BruteForceByBitsTF(10, func(b []bool) { fmt.Println(b) }
-// -> [false, false, false, false, false, false, false, false, false, false]
-//    [true, false, false, false, false, false, false, false, false, false]
-//    [false, true, false, false, false, false, false, false, false, false]
-//    ...
-func BruteForceByBitsTF(n int, fn func([]bool)) {
-	N := 1 << uint(n)
-	a := make([]bool, n)
+// BruteForceByBitsTF(bitsNum, fn) calls fn with []bool for each n-bit true/false pattern
+func BruteForceByBitsTF(bitsNum int, fn func(bitFlags []bool)) {
+	// e.g.
+	// BruteForceByBitsTF(10, func(b []bool) { fmt.Println(b) }
+	// -> [false, false, false, false, false, false, false, false, false, false]
+	//    [true, false, false, false, false, false, false, false, false, false]
+	//    [false, true, false, false, false, false, false, false, false, false]
+	//    ...
+	N := 1 << uint(bitsNum)
+	a := make([]bool, bitsNum)
 	for i := 0; i < N; i++ {
-		for j := 0; j < n; j++ {
-			k := n - j - 1
+		for j := 0; j < bitsNum; j++ {
+			k := bitsNum - j - 1
 			if i&(1<<uint(j)) == 0 {
 				a[k] = false
 			} else {
